a5a56e359e5ae2de2cfc36ca5aeb7fec6f04e6c1
syahban-afk <syahbansyahputrati@gmail.com>
2025-06-04 14:44:27 +0700

    memperbaiki tampilan page

--- resources/views/shop/about.blade.php
+++ resources/views/shop/about.blade.php
@@ -1,7 +1,7 @@
 <x-shop-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white dark:bg-gradient-to-tl from-gray-800 via-gray-900 to-gray-900 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <!-- Hero Section -->
                     <section class="mb-16 text-center">
@@ -32,6 +32,12 @@
                         <div class="flex flex-col gap-4">
 
                             <div class="flex gap-4">
+
+                                <div class="w-2/3 aspect-[3/4] overflow-hidden rounded-[15%]">
+                                    <img src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                                        alt="Kue" class="w-full h-full object-cover">
+                                </div>
+
                                 <div class="w-1/3 flex flex-col gap-4">
                                     <div class="aspect-square overflow-hidden rounded-full">
                                         <img src="https://images.unsplash.com/photo-1550317138-10000687a72b?auto=format&fit=crop&w=800&q=80"
@@ -41,11 +47,6 @@
                                         <img src="https://images.pexels.com/photos/842571/pexels-photo-842571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                                             alt="Pancake" class="w-full h-full object-cover">
                                     </div>
-                                </div>
-
-                                <div class="w-2/3 aspect-[3/4] overflow-hidden rounded-[15%]">
-                                    <img src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                                        alt="Kue" class="w-full h-full object-cover">
                                 </div>
 
                             </div>
@@ -58,7 +59,7 @@
                         <h2 class="text-2xl font-bold mb-8 text-center">Nilai-Nilai Kami</h2>
                         <div class="grid md:grid-cols-3 gap-8">
                             <div class="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg">
-                                <div class="text-indigo-600 dark:text-indigo-400 mb-4">
+                                <div class="ttext-indigo-600 dark:text-indigo-400 dark:drop-shadow-[0_0_4px_#818cf8] transition-all mb-4">
                                     <svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                             d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z">
@@ -70,7 +71,7 @@
                             </div>
 
                             <div class="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg">
-                                <div class="text-indigo-600 dark:text-indigo-400 mb-4">
+                                <div class="ttext-indigo-600 dark:text-indigo-400 dark:drop-shadow-[0_0_4px_#818cf8] transition-all mb-4">
                                     <svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                             d="M13 10V3L4 14h7v7l9-11h-7z"></path>
@@ -81,7 +82,7 @@
                             </div>
 
                             <div class="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg">
-                                <div class="text-indigo-600 dark:text-indigo-400 mb-4">
+                                <div class="ttext-indigo-600 dark:text-indigo-400 dark:drop-shadow-[0_0_4px_#818cf8] transition-all mb-4">
                                     <svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                             d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z">
